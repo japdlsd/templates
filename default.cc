d7c1a53cc586868c954033bcd24e2bcefcf0fe2d
Askar Gafurov <japdlsd@gmail.com>
2014-02-05 21:12:29 +0100

    template functions, aliases and basic .cc template and makefile.

---
+++ default.cc
@@ -0,0 +1,11 @@
+// Dark Side of Elephant
+// Askar
+
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main(){
+    
+}
---
+++ default.py
@@ -0,0 +1,5 @@
+# Dark Side of Elephant
+# Askar
+
+import sys
+sys.setrecursionlimit(1000000)
